4dc7f49e038a69d351053d69bee2599a1d62a367
ram <ramkrishan@kontikilabs.com>
2019-03-16 02:03:51 +0530

    Updates

---
+++ precache-manifest.0c5c42fdf3063173f4c9694547756ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c1cd607eab9c343de68",
+    "url": "/Keycode/static/js/runtime~main.6dbab00c.js"
+  },
+  {
+    "revision": "317ca2c9b64b47a76d7f",
+    "url": "/Keycode/static/js/main.d1a0808f.chunk.js"
+  },
+  {
+    "revision": "e3022bf856811f70ddd5",
+    "url": "/Keycode/static/js/2.760b29d1.chunk.js"
+  },
+  {
+    "revision": "317ca2c9b64b47a76d7f",
+    "url": "/Keycode/static/css/main.e1a693f6.chunk.css"
+  },
+  {
+    "revision": "e3022bf856811f70ddd5",
+    "url": "/Keycode/static/css/2.ce1cca51.chunk.css"
+  },
+  {
+    "revision": "7b1f43bee1831dfa8855e6f5df938966",
+    "url": "/Keycode/index.html"
+  }
+];
